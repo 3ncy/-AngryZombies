aa46a75fbacfa5136fa302bc083172900f9f836e
encyk <martiiin225@email.cz>
2022-01-31 15:19:49 +0100

    fixed zombie attacking

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -10,16 +10,11 @@ public class EnemyController : MonoBehaviour
     [SerializeField] Transform player;
     [SerializeField] NavMeshAgent agent;
 
-    Weapon punch = new Weapon
-    {
-        Firerate = 4,
-        Damage = 10
-    };
-    private float timeSinceLastPunch = 0;
+    [SerializeField] float damage = 10;
 
     //mby add ragdolls?
-    //[SerializeField] Collider mainCollider;//todo: predelat na list collidery, incase velky enemy je postaveny z vice tvaru
-    [SerializeField] Rigidbody rb;
+
+    //[SerializeField] Rigidbody rb;
     [SerializeField] Animator animator;
 
     // Start is called before the first frame update
@@ -33,27 +28,19 @@ public class EnemyController : MonoBehaviour
     {
         agent.SetDestination(player.position);
 
+        animator.SetBool("Running", agent.velocity.magnitude > 0);
 
-        animator.SetBool("Running", agent.velocity.magnitude > 0); //todo: mby setovat jenom pokud se zacina/prestava hybat, mozna pomuze optimalizaci
+        animator.SetBool("Attacking", agent.remainingDistance < 1.5f); //todo: mby vypnout animace, po smrti hrace
+       
 
-        //todo: je tu offset animace a kdy se actually dava dmg.
-        //mby courotines? idk jdu spat
-
-        bool attacking = agent.remainingDistance < 1.5f;
-        animator.SetBool("Attacking", attacking);
-        timeSinceLastPunch += Time.deltaTime;// actually mozna neni potreba a hodilo by se to opacne. For now to nebudu resit a uvidi se, jake problemy to dela pri balancovani// * (attacking ? 1 : 0); //resetuje punch cooldown kdyz zrovna zombie neni v attack distance
-        if (attacking && timeSinceLastPunch > punch.Firerate)
-        {
-            timeSinceLastPunch = 0;
-            PlayerController playerController = player.GetComponent<PlayerController>();
-            if (playerController != null) playerController.TakeDamage(punch.Damage);
-            Debug.LogWarning(gameObject.name + " Hit player");
-
-        }
-        
         //agent.enabled = rb.isKinematic = rb.velocity.magnitude < 0.01;  // vypnout agenta a zapnout RB pokud je nejaka force na rb
+    }
 
-
+    public void Attack()
+    {
+        PlayerController playerController = player.GetComponent<PlayerController>();
+        if (playerController != null) playerController.TakeDamage(damage);
+        Debug.LogWarning(gameObject.name + " Hit player");
     }
 
     public void TakeDamage(float damage)
@@ -61,18 +48,18 @@ public class EnemyController : MonoBehaviour
         health -= damage;
         if (health <= 0)
         {
-            //rb.isKinematic = false;
             agent.enabled = false;
             animator.SetTrigger("Died");
-            //mainCollider.enabled = false;
-            //rb.useGravity = false;
             foreach (Collider c in GetComponents<Collider>())
             {
                 c.enabled = false;
             }
+            transform.position -= new Vector3(0, 0.05f, 0); //dirty fix na levitujici mrtvoly
             Destroy(this);    //this.enabled = false;
+            //rb.isKinematic = false;
+            //rb.useGravity = false;
             //rb.constraints = RigidbodyConstraints.FreezeAll;
-            //Destroy(this, 5); //mby
+            Destroy(gameObject, 5); //zmizeni mrtvoly po 5s 
         }
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,6 +7,7 @@ public struct Weapon
 {
     public float Firerate; //je to v sekundach, mby predelat do RPM
     public float Damage;
+    public float Range;
     public ParticleSystem MuzzleFlash;
     public GameObject ImpactEffect;
 }
@@ -25,6 +26,9 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private Animator animator;
     [SerializeField] private Rigidbody rb;
 
+    private float horizontalInput;
+    private float verticalInput;
+
     // Start is called before the first frame update
     void Start()
     { }
@@ -32,8 +36,6 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //TODO: rozhazet do metod Move(); ANimations(); Shoot();, nebo mby HandleInput(); a ANimations();
-
         timeSinceLastShot += Time.deltaTime;
         if (Input.GetButton("Fire1") && timeSinceLastShot >= weapons[selectedWeaponIndex].Firerate)//todo: pridat moznost ze nektere zbrane jsou single fire
         {
@@ -41,8 +43,7 @@ public class PlayerController : MonoBehaviour
             weapons[selectedWeaponIndex].MuzzleFlash.Play();
 
             RaycastHit hit;
-            if (Physics.Raycast(shotsOrigin.position, shotsOrigin.forward, out hit))
-            //if (Physics.Raycast(transform.position, transform.forward, out hit)) //todo: mby, chce to vic testing
+            if (Physics.Raycast(shotsOrigin.position, shotsOrigin.forward, out hit, weapons[selectedWeaponIndex].Range))
             {
                 //todo: jinej particle eff pokud dopadne hit na zombie 
                 //todo: udelat ten vfx
@@ -56,6 +57,7 @@ public class PlayerController : MonoBehaviour
                     if (enemy != null)
                     {
                         enemy.TakeDamage(weapons[selectedWeaponIndex].Damage);
+                        
                     }
 
                     //pridani knockbacku, mby udelat l8tr
@@ -68,20 +70,15 @@ public class PlayerController : MonoBehaviour
         }
 
 
-        float horizontalInput = Input.GetAxis("Horizontal");
-        float verticalInput = Input.GetAxis("Vertical");
-
-
-        Vector3 pohyb = new Vector3(horizontalInput, 0, verticalInput);
-        pohyb.Normalize();
-        pohyb *= speed * Time.deltaTime;
-        rb.MovePosition(transform.position + pohyb);
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
         //transform.Translate(pohyb, Space.World);
 
         //ANIMACE
         float otoceni = Mathf.Atan2(transform.forward.z, transform.forward.x); //v Rad
         float uhelChuze = Mathf.Atan2(verticalInput, horizontalInput); //v Rad
         float rozdil = otoceni - uhelChuze; //v Rad
+
 
         if (horizontalInput == 0 && verticalInput == 0)
         {
@@ -109,9 +106,18 @@ public class PlayerController : MonoBehaviour
 
     }
 
-    public void TakeDamage(float damage)
+    public void FixedUpdate()
     {
 
+        Vector3 pohyb = new Vector3(horizontalInput, 0, verticalInput);
+        pohyb.Normalize();
+        pohyb *= speed * Time.fixedDeltaTime;
+        rb.MovePosition(transform.position + pohyb);
+    }
+
+
+    public void TakeDamage(float damage)
+    {
         // ridat red screen overlay pri hitu
 
         health -= damage;
